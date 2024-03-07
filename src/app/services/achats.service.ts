e2e83a8a2ab249092b743761fbacb5357243e462
aquaman79 <qquamar1@gmail.com>
2024-03-07 17:23:16 +0100

    ajout d'achat

--- src/app/services/achats.service.ts
+++ src/app/services/achats.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Film } from '../modele/film'; // Assurez-vous que le chemin est correct
 
@@ -10,13 +10,17 @@ export class AchatsService {
   private baseUrl = '/api/v1'; // Remplacez par l'URL de votre API
 
   constructor(private http: HttpClient) { }
-
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer '+localStorage.getItem('access_token') || '' // Provide a fallback value
+  });
+ options = { headers: this.headers };
   postAchat(panier: Film[], id : any ): Observable<any> {
-    return this.http.post(`${this.baseUrl}/transactions/${id}`, panier);
+    return this.http.post(`${this.baseUrl}/transactions/${id}`, panier,this.options);
   }
 
   // Autres méthodes pour interagir avec le backend, par exemple :
   getAchat(id : any ): Observable<any> {
-    return this.http.get(`${this.baseUrl}/transactions/${id}`);
+    return this.http.get(`${this.baseUrl}/transactions/${id}`,this.options);
   } 
 }
