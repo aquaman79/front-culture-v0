d81132a463d3545f298d738d3d5c5bb9f5de67fc
aquaman79 <qquamar1@gmail.com>
2024-03-07 15:44:25 +0100

    modification service panier

--- src/app/services/panier.service.ts
+++ src/app/services/panier.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Film } from '../modele/film';
@@ -11,15 +11,19 @@ export class PanierService {
   private baseUrl = '/api/v1/'; // Remplacez par l'URL de votre API
 
   constructor(private http: HttpClient) {}
-
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer '+localStorage.getItem('access_token') || '' // Provide a fallback value
+  });
+ options = { headers: this.headers };
   postPanier(panier: any, id: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}/paniers/${id}`, panier);
+    return this.http.post(`${this.baseUrl}/paniers/${id}`, panier,this.options);
   }
   
 
   // Autres méthodes pour interagir avec le backend, par exemple :
   getPanier(id : any ): Observable<any> {
-    return this.http.get(`${this.baseUrl}/paniers/${id}`);
+    return this.http.get(`${this.baseUrl}/paniers/${id}`,this.options);
   }
 
 }
