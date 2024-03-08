9d6a9af14c21c2eb1701ff6ee054d72047424b63
aquaman79 <qquamar1@gmail.com>
2024-03-08 14:43:48 +0100

    modification ajoute commentaire

--- src/app/achat/achat.component.ts
+++ src/app/achat/achat.component.ts
@@ -1,6 +1,6 @@
 import { AchatsService } from './../services/achats.service';
 import { Component } from '@angular/core';
-import { Film } from '../modele/film';
+import { Commentaire, Film } from '../modele/film';
 import { FilmService } from '../services/film.service';
 @Component({
   selector: 'app-achat',
@@ -50,18 +50,49 @@ export class AchatComponent {
 
   }
 
-  ajouteCommentaire(film: any ){
-    this.filmService.modifieFilm(film,this.id).subscribe({
-      next: (response) => {
-        console.log('Films ajoutés avec succès', response);
-        // Vous pouvez ici gérer la réinitialisation du formulaire ou d'autres actions de succès.
-      },
-      error: (error) => {
-        console.error('Erreur lors de l\'ajout des films', error);
-        // Gérer les erreurs ici
+  newCommentAuthor: string = '';
+  newCommentContent: string = '';
+
+  onSubmitComment() {
+    if (this.selectedFilm) { // Assurez-vous que selectedFilm n'est pas null
+      const newComment: Commentaire = {
+        id : 0 , // Laissez le backend définir l'ID
+        auteur: this.newCommentAuthor,
+        contenu: this.newCommentContent,
+        date: new Date(), // Définissez la date actuelle, si votre backend ne le fait pas
+      };
+  
+      // Vérification si la liste des commentaires existe, sinon la créer
+      if (!this.selectedFilm.comments) {
+        this.selectedFilm.comments = [];
       }
-    });;
+  
+      // Ajout du nouveau commentaire à la liste des commentaires du film sélectionné
+      this.selectedFilm.comments.push(newComment);
+  
+      // Appel de la méthode pour mettre à jour le film avec le nouveau commentaire
+      this.ajouteCommentaire(this.selectedFilm);
+    
+      // Réinitialisation des champs du formulaire
+      this.newCommentAuthor = '';
+      this.newCommentContent = '';
+    }
   }
+  
+  ajouteCommentaire(film: Film) {
+    if (film.id) { // Vérifiez que le film a un ID valide
+      this.filmService.modifieFilm(film, this.id).subscribe({
+        next: (response) => {
+          console.log('Film modifié avec succès', response);
+          // Ici, vous pourriez vouloir mettre à jour l'interface utilisateur en conséquence
+        },
+        error: (error) => {
+          console.error('Erreur lors de la modification du film', error);
+        }
+      });
+    }
+  }
+  
 
 
 }
